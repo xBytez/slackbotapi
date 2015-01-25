6458ccf624043da2b9b80d0668b1ca220f766a36
xBytez <me@xbytez.eu>
2015-01-25 15:52:51 +0100

    Added comments to example bot.

--- example_bot.js
+++ example_bot.js
@@ -1,11 +1,11 @@
 //////////////////////////////////////////////////////////////////
-//			███████╗██╗      █████╗  ██████╗██╗  ██╗ 			//
-//			██╔════╝██║     ██╔══██╗██╔════╝██║ ██╔╝ 			//
-//			███████╗██║     ███████║██║     █████╔╝  			//
-//			╚════██║██║     ██╔══██║██║     ██╔═██╗  			//
-//			███████║███████╗██║  ██║╚██████╗██║  ██╗ 			//
-//			╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ 			//
-//			    14-12-14 | xBytez | me@xbytez.eu				//
+//		███████╗██╗      █████╗  ██████╗██╗  ██╗ 	//
+//		██╔════╝██║     ██╔══██╗██╔════╝██║ ██╔╝ 	//
+//		███████╗██║     ███████║██║     █████╔╝ 	//
+//		╚════██║██║     ██╔══██║██║     ██╔═██╗		//
+//		███████║███████╗██║  ██║╚██████╗██║  ██╗	//
+//		╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝	//
+//		    14-12-14 | xBytez | me@xbytez.eu		//
 //////////////////////////////////////////////////////////////////
 
 // Requiring our module
@@ -14,20 +14,30 @@ var slackAPI = require('slackAPI');
 // Starting
 var slack = new slackAPI("TOKENHERE");
 
+// Slack on EVENT message, send data.
 slack.on('message', function(data) {
+	// If no text, return.
 	if(typeof data.text == 'undefined') return;
+	// If someone says 'cake!!' respond to their message with "@user OOH, CAKE!! :cake"
 	if(data.text === 'cake!!') slack.sendMsg(data.channel, "@"+slack.getUser(data.user).name+" OOH, CAKE!! :cake:")
+	
+	// If the first character starts with %, you can change this to your own prefix of course.
 	if(data.text.charAt(0) === '%') {
+		// Split the command and it's arguments into an array
 		var command = data.text.substring(1).split(' ');
 
+		// If command[2] is not undefined use command[1] to have all arguments in comand[1]
 		if (typeof command[2] != "undefined") {
 			for (var i = 2; i < command.length; i++) {
 				command[1] = command[1] + ' ' + command[i];
 			}
 		}
 
+		// Switch to check which command has been requested.
 		switch (command[0].toLowerCase()) {
+			// If hello
 			case "hello":
+				// Send message.
 				slack.sendMsg(data.channel, "Oh, hello @"+slack.getUser(data.user).name+" !")
 			break;
 
